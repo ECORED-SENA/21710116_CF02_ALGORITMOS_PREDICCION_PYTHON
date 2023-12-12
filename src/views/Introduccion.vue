1e7be9d3d27e6020e1c59d8fce414ddb9d874a18
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-12-12 13:03:36 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -18,7 +18,7 @@
     
     figure(data-aos="fade-left")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/aB_4x9uOhgA?si=ck68peWWVljDGvy0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/aB_4x9uOhgA?si=cSYsL2qG1H5A_xZQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
